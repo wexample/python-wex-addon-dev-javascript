699595ddcedf081adf70a96d92e237dcf8e82d20
weeger <romain.weeger@gmail.com>
2025-08-29 08:52:29 +0200

    Rect

--- src/wexample_wex_addon_dev_javascript/workdir/javascript_package_workdir.py
+++ src/wexample_wex_addon_dev_javascript/workdir/javascript_package_workdir.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 class JavascriptPackageWorkdir:
     """Workdir placeholder for a JavaScript package."""
     pass
--- src/wexample_wex_addon_dev_javascript/workdir/javascript_packages_suite_workdir.py
+++ src/wexample_wex_addon_dev_javascript/workdir/javascript_packages_suite_workdir.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 class JavascriptPackagesSuiteWorkdir:
     """Workdir placeholder for a suite of JavaScript packages."""
     pass
