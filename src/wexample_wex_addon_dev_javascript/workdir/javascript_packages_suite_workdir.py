d5066b431cea39522fed5557265861381e0acbdc
weeger <romain.weeger@gmail.com>
2025-08-29 09:19:38 +0200

    Base config

--- src/wexample_wex_addon_dev_javascript/workdir/javascript_packages_suite_workdir.py
+++ src/wexample_wex_addon_dev_javascript/workdir/javascript_packages_suite_workdir.py
@@ -1,9 +1,20 @@
 from __future__ import annotations
 
+from typing import TYPE_CHECKING
+
 from wexample_wex_core.workdir.framework_packages_suite_workdir import (
     FrameworkPackageSuiteWorkdir,
 )
 
+if TYPE_CHECKING:
+    from wexample_wex_core.workdir.code_base_workdir import CodeBaseWorkdir
+
+
 class JavascriptPackagesSuiteWorkdir(FrameworkPackageSuiteWorkdir):
-    """Workdir placeholder for a suite of JavaScript packages."""
-    pass
+
+    def _get_children_default_workdir_class(self) -> type[CodeBaseWorkdir]:
+        from wexample_wex_addon_dev_javascript.workdir.javascript_package_workdir import (
+            JavascriptPackageWorkdir,
+        )
+
+        return JavascriptPackageWorkdir
