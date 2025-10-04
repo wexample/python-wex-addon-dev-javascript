ddb806dec2b6f24568c2980bf4e088dfa5d6b4a4
weeger <romain.weeger@gmail.com>
2025-10-04 20:32:05 +0200

    Move wex workdir

--- src/wexample_wex_addon_dev_javascript/workdir/javascript_workdir.py
+++ src/wexample_wex_addon_dev_javascript/workdir/javascript_workdir.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from wexample_wex_core.workdir.code_base_workdir import CodeBaseWorkdir
+from wexample_wex_addon_app.workdir.code_base_workdir import CodeBaseWorkdir
 
 
 class JavascriptWorkdir(CodeBaseWorkdir):
