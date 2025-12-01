6571b25759da9ad76864d1eaf307742e5b8a9c4e
weeger <romain.weeger@gmail.com>
2025-12-01 15:51:31 +0100

    Fix dependencies

--- src/wexample_wex_addon_dev_javascript/workdir/javascript_workdir.py
+++ src/wexample_wex_addon_dev_javascript/workdir/javascript_workdir.py
@@ -4,6 +4,6 @@ from wexample_wex_addon_app.workdir.code_base_workdir import CodeBaseWorkdir
 
 
 class JavascriptWorkdir(CodeBaseWorkdir):
-    def get_dependencies(self) -> list[str]:
+    def get_dependencies_versions(self) -> dict[str, str]:
         # TODO search in package.json
-        return []
+        return {}
