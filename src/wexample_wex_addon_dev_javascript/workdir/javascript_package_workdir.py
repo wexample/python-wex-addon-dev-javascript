6bcb8a41270f453a3213790b4c3c27badbc21bb2
weeger <romain.weeger@gmail.com>
2025-12-02 08:04:24 +0100

    Publish

--- src/wexample_wex_addon_dev_javascript/workdir/javascript_package_workdir.py
+++ src/wexample_wex_addon_dev_javascript/workdir/javascript_package_workdir.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
+import os
+import subprocess
 from typing import TYPE_CHECKING
 
 from wexample_wex_addon_dev_javascript.workdir.javascript_workdir import (
@@ -29,3 +31,56 @@ class JavascriptPackageWorkdir(JavascriptWorkdir):
         )
 
         return JavascriptPackagesSuiteWorkdir
+
+    def _publish(self, force: bool = False) -> None:
+        """Publish the package to npm, skipping if the version already exists."""
+        from wexample_helpers.helpers.shell import shell_run
+
+        package_name = self.get_package_name()
+        version = self.get_project_version()
+        registry = (
+            self.get_env_parameter_or_suite_fallback("NPM_REGISTRY")
+            or "https://registry.npmjs.org"
+        )
+
+        # Detect if the version already exists on the registry
+        release_exists = False
+        try:
+            shell_run(
+                [
+                    "npm",
+                    "view",
+                    f"{package_name}@{version}",
+                    "version",
+                    "--registry",
+                    registry,
+                ],
+                cwd=self.get_path(),
+                inherit_stdio=False,
+            )
+            release_exists = True
+        except subprocess.CalledProcessError:
+            release_exists = False
+
+        if release_exists and not force:
+            self.warning(
+                f'Trying to publish an existing release for package "{package_name}" version {version}'
+            )
+            return
+
+        token = self.get_env_parameter_or_suite_fallback("NPM_TOKEN")
+        env = os.environ.copy()
+        host = registry.rstrip("/").split("://")[-1]
+
+        if registry:
+            env["npm_config_registry"] = registry
+        if token:
+            env["NPM_TOKEN"] = token
+            env[f"npm_config_//{host}/:_authToken"] = token
+
+        shell_run(
+            ["npm", "publish", "--registry", registry],
+            cwd=self.get_path(),
+            env=env,
+            inherit_stdio=True,
+        )
